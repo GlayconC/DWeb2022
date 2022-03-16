dfb2033021c67fb5fb7aa0b3757a1e546104cbd0
GlayconC <88117268+GlayconC@users.noreply.github.com>
2022-03-16 14:22:26 -0300

    Não terminado

---
+++ CrudDWeb2/autoload.php
@@ -0,0 +1,8 @@
+<?php
+spl_autoload_register(function($class) {
+    $file = $_SERVER["DOCUMENT_ROOT"] . '/CrudDWeb2/libs/' . $class . '.class.php';
+ 
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
